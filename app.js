7277beb101e720a10dcc434a25266eed9a3cb45b
vikel <vikelshay@gmail.com>
2021-04-19 17:28:59 -0700

    .

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const cors = require('cors');
+
 
 var indexRouter = require('./src/routes');
 var usersRouter = require('./src/routes/users');
@@ -9,6 +11,7 @@ var diagnocatRouter = require('./src/routes/diagnocat');
 
 var app = express();
 
+app.use(cors);
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
