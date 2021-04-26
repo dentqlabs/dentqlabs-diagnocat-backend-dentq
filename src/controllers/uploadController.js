e2bb7bdc9e4107f1cfc896a28e93f4c2d84d345f
vikel <vikelshay@gmail.com>
2021-04-25 20:38:57 -0700

    process req

--- src/controllers/uploadController.js
+++ src/controllers/uploadController.js
@@ -10,52 +10,58 @@ exports.startUpload = async function(req, res) {
         res.status(400).send('No file uploaded.');
         return;
     }
-    console.log(req.file);
-    return openSession(req, res);
+
+    const uid = {uid: "a4b51645-639a-7dc8-89b2-260e43ded522"}; //TODO: get from req
+    // console.log(req.file);
+
+    try {
+
+        //1. open session
+        let openSessionResponse = await openSession(uid);
+        let sessionId = openSessionResponse.data.session_id;
+        // console.log(sessionId);
+
+        //2. request upload urls
+        const uploadUrlsRequest = {
+            session_id: sessionId,
+            keys: ["file1"]
+        }
+
+        let a = await requestUploadUrls(uploadUrlsRequest);
+        // console.log(a.data.upload_urls);
+
+        let diagnocatStorageUrl = a.data.upload_urls[0];
+
+        //3. for each url - upload file
+        let googleResponse = await uploadFiles(uploadUrlsRequest, req.file);
+        console.log(googleResponse);
+        console.log('googleResponse');
+
+        res.send(sessionId);
+    } catch (e) {
+        res.send(e);
+    }
+
 }
 
 //1. Open uploading session:
 //Session expires in 5 minutes in case of absence of progress-notify requests.
 //TODO: option to add study to req JSON
 exports.openSession = openSession;
-
-async function openSession(req, res) {
-    const uid = req.body;
-    console.log(uid);
-    instance.post(DIAGNOCAT_UPLOAD_OPEN_SESSION,uid, { headers:  {'Content-Type': 'application/json'}})
-        .then(response => {
-            console.log(response.data);
-            res.send(response.data);
-        })
-        .catch((e) => {
-            res.send(e);
-        });
+async function openSession(uid) {
+    return instance.post(DIAGNOCAT_UPLOAD_OPEN_SESSION,uid, { headers:  {'Content-Type': 'application/json'}});
 }
 
 //2. Add files to session:
-exports.requestUploadUrls = async function(req, res) {
-    const {session_id, keys} = req.body;
-    instance.post(DIAGNOCAT_UPLOAD_REQUEST_UPLOAD_URLS, {session_id, keys}, { headers:  {'Content-Type': 'application/json'}})
-        .then(response => {
-            res.send(response.data);
-            //TODO: uploadFiles
-        })
-        .catch((e) => {
-            res.send(e);
-        });
+exports.requestUploadUrls = requestUploadUrls;
+async function requestUploadUrls(uploadUrlsRequest) {
+    const {session_id, keys} = uploadUrlsRequest;
+    return instance.post(DIAGNOCAT_UPLOAD_REQUEST_UPLOAD_URLS, {session_id, keys}, { headers:  {'Content-Type': 'application/json'}});
 }
 
 //2.1. PUT request should be sent to a URL received via request-upload-urls, in order to upload the files.
-uploadFiles = async function(req, res) {
-    let diagnocatStorageUrl = req.url;
-    let file = {}; //??
-    axios.put(diagnocatStorageUrl, file, { headers:  {'Content-Type': 'application/json'}})
-        .then(response => {
-            res.send(response.data);
-        })
-        .catch((e) => {
-            res.send(e);
-        });
+uploadFiles = async function(uploadUrlsRequest, file) {
+    return axios.put(uploadUrlsRequest, file, { headers:  {'Content-Type': 'application/json'}});
 }
 
 
