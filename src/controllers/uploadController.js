76ccba49b62ba0e1d2cdad0de76acba915823f33
vikel <vikelshay@gmail.com>
2021-04-25 19:46:01 -0700

    upload file using multer

--- src/controllers/uploadController.js
+++ src/controllers/uploadController.js
@@ -1,16 +1,30 @@
+const axios = require('axios');
 const instance = require('../axios').v1_instance;
 
 const DIAGNOCAT_UPLOAD_OPEN_SESSION = '/upload/open-session';
 const DIAGNOCAT_UPLOAD_REQUEST_UPLOAD_URLS = '/upload/request-upload-urls';
+const DIAGNOCAT_UPLOAD_PROGRESS_NOTIFY = '/upload/progress-notify';
+
+exports.startUpload = async function(req, res) {
+    if (!req.file) {
+        res.status(400).send('No file uploaded.');
+        return;
+    }
+    console.log(req.file);
+    return openSession(req, res);
+}
 
 //1. Open uploading session:
 //Session expires in 5 minutes in case of absence of progress-notify requests.
 //TODO: option to add study to req JSON
-exports.openSession = async function(req, res) {
+exports.openSession = openSession;
+
+async function openSession(req, res) {
     const uid = req.body;
     console.log(uid);
     instance.post(DIAGNOCAT_UPLOAD_OPEN_SESSION,uid, { headers:  {'Content-Type': 'application/json'}})
         .then(response => {
+            console.log(response.data);
             res.send(response.data);
         })
         .catch((e) => {
@@ -24,8 +38,41 @@ exports.requestUploadUrls = async function(req, res) {
     instance.post(DIAGNOCAT_UPLOAD_REQUEST_UPLOAD_URLS, {session_id, keys}, { headers:  {'Content-Type': 'application/json'}})
         .then(response => {
             res.send(response.data);
+            //TODO: uploadFiles
         })
         .catch((e) => {
             res.send(e);
         });
 }
+
+//2.1. PUT request should be sent to a URL received via request-upload-urls, in order to upload the files.
+uploadFiles = async function(req, res) {
+    let diagnocatStorageUrl = req.url;
+    let file = {}; //??
+    axios.put(diagnocatStorageUrl, file, { headers:  {'Content-Type': 'application/json'}})
+        .then(response => {
+            res.send(response.data);
+        })
+        .catch((e) => {
+            res.send(e);
+        });
+}
+
+
+//3. Notify the server of upload progress, if needed
+/**
+ * Notify server of progress being made in uploading session.
+ * Client is required to periodically call this method (recommended interval is 5 seconds).
+ * While session is active, client should call this method at least every 30 seconds.
+ */
+exports.uploadProgressNotify = async function(req, res) {
+    const {session_id, keys} = req.body;
+    instance.post(DIAGNOCAT_UPLOAD_PROGRESS_NOTIFY, {session_id, keys}, { headers:  {'Content-Type': 'application/json'}})
+        .then(response => {
+            res.send(response.data);
+        })
+        .catch((e) => {
+            res.send(e);
+        });
+}
+
--- src/routes/diagnocat.js
+++ src/routes/diagnocat.js
@@ -1,18 +1,31 @@
 const diagnocatController = require('../controllers/diagnocatController');
 const uploadController = require('../controllers/uploadController');
+const Multer = require('multer');
+
+
+// Multer is required to process file uploads and make them available via
+// req.files.
+const multer = Multer({
+    storage: Multer.memoryStorage(),
+    limits: {
+        fileSize: 500 * 1024 * 1024, // no larger than 500mb.
+    },
+});
 
 const express = require('express');
 const router = express.Router();
 
 router.get('/patients', diagnocatController.getPatients);
 router.post('/patients', diagnocatController.createPatient);
-
 router.get('/patients/:uid/studies', diagnocatController.getPatientStudies);
+
 router.get('/studies/:studyUID/images', diagnocatController.getStudyImages);
 router.get('/studies/:studyUID/analyses', diagnocatController.getStudyAnalyses);
 
+router.post('/upload/start-upload',multer.single('file'), uploadController.startUpload);
 
 router.post('/upload/open-session', uploadController.openSession);
 router.post('/upload/request-upload-urls', uploadController.requestUploadUrls);
+router.post('/upload/progress-notify', uploadController.uploadProgressNotify);
 
 module.exports = router;
